d12cca012278d306804028d72c14d9fd088903c2
mxlnk <maxi.link@octomind.dev>
2023-11-30 15:11:02 +0100

    Update keymap.c for :apple:

--- keyboards/kyria/keymaps/mxlnk/keymap.c
+++ keyboards/kyria/keymaps/mxlnk/keymap.c
@@ -25,23 +25,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
  * Base Layer: QWERTY
  *
- * ,-------------------------------------------.                              ,-------------------------------------------.
- * | ESC    |   Q  |   W  |   E  |   R  |   T  |                              |   Y  |   U  |   I  |   O  |   P  |  | \   |
- * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * | LSFT   |   A  |   S  |  D   |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |  ' "   |
- * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * | LCTL   |   Z  |   X  |   C  |   V  |   B  | Enter|  (   |  |  )   | Del  |   N  |   M  | ,  < | . >  | /  ? |  - _   |
- * |        |      |      |      |      |      | NAV  |      |  |      | NAV  |      |      |      |      |      |        |
- * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        | GUI  | Alt  |  NUM | Space|  {[  |  |  ]}  | Bspc |  NUM | Tab  | ENTER|
- *                        |      |      |  SYM | Shift| Shift|  | Shift| Shift|  SYM |      |      |
- *                        `----------------------------------'  `----------------------------------'
+ * ,-------------------------------------------.                                ,-------------------------------------------.
+ * | ESC    |   Q  |   W  |   E  |   R  |   T  |                                |   Y  |   U  |   I  |   O  |   P  |  | \   |
+ * |--------+------+------+------+------+------|                                |------+------+------+------+------+--------|
+ * | LSFT   |   A  |   S  |  D   |   F  |   G  |                                |   H  |   J  |   K  |   L  | ;  : |  ' "sft|
+ * |--------+------+------+------+------+------+--------------.  ,--------------+------+------+------+------+------+--------|
+ * | LCTL   |   Z  |   X  |   C  |   V  |   B  | Enter |  (   |  |  )   | Del   |   N  |   M  | ,  < | . >  | /  ? |  - _   |
+ * |        |      |      |      |      |      | NAV   |      |  |      | NAV   |      |      |      |      |      |        |
+ * `----------------------+------+------+------+-------+------|  |------+-------+------+------+------+----------------------'
+ *                        | GUI  | CMD  |  Alt | Space |  {[  |  |  ]}  | Bspc  |  Tab | CMD  | ENTER|
+ *                        |      |      |      |SYM/NUM|      |  |      |SYM/NUM|      |      |      |
+ *                        `-----------------------------------'  `-----------------------------------'
  */
     [QWERTY] = LAYOUT(
       KC_ESC,  KC_Q,   KC_W,   KC_E,    KC_R,    KC_T,                                                                                       KC_Y,      KC_U,      KC_I,   KC_O,    KC_P,    KC_BSLASH,
-      KC_LSFT, KC_A,   KC_S,   KC_D,    KC_F,    KC_G,                                                                                       KC_H,      KC_J,      KC_K,   KC_L,    KC_SCLN, KC_QUOT,
-      KC_LCTL, KC_Z,   KC_X,   KC_C,    KC_V,    KC_B,      LT(RAISE, KC_ENT), KC_LPRN,             KC_RPRN,             LT(RAISE, KC_DEL),  KC_N,      KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
-                               KC_LGUI, KC_LALT, OSL(LOWER), LSFT_T(KC_SPC),    LSFT_T(KC_LBRACKET), LSFT_T(KC_RBRACKET), LSFT_T(KC_BSPC),    OSL(LOWER), KC_TAB, KC_ENT
+      KC_LSFT, KC_A,   KC_S,   KC_D,    KC_F,    KC_G,                                                                                       KC_H,      KC_J,      KC_K,   KC_L,    KC_SCLN, LSFT_T(KC_QUOT),
+      KC_LCTL, KC_Z,   KC_X,   KC_C,    KC_V,    KC_B,    LT(RAISE, KC_ENT), KC_LPRN,         KC_RPRN, LT(RAISE, KC_DEL), KC_N,      KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
+                               KC_LGUI, KC_LCMD, KC_LALT, LT(LOWER,KC_SPC),  KC_LBRACKET, KC_RBRACKET, LT(LOWER,KC_BSPC), KC_TAB, KC_RCMD,   KC_ENT
     ),
 /*
  * Numpad, Symbols (Lower)
